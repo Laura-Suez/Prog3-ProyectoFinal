a1025b654b730917a58f5d0cf7eb937b2e16eedf
Laura-Suez <laura.suez@outlook.com>
2026-04-15 17:20:09 -0300

    Proyecto Inicializado y arreglo inicial de productos cargado

---
+++ CellphoneStore/src/App.jsx
@@ -0,0 +1,126 @@
+function App() {
+  // const productList = [
+  //   {
+  //     id: 1,
+  //     name: "Redmi Buds 6",
+  //     image: "/accessories/AirpodsBluetoothRedmiBuds6.webp",
+  //     category: "Accessories",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Redmi Buds 8 White",
+  //     image: "/accessories/AudífonosInalámbricosXiaomiRedmiBuds8.webp",
+  //     category: "Accessories",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Redmi Buds Black",
+  //     image: "/accessories/AuricularesXiaomiRedmi.webp",
+  //     category: "Accessories",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "Xiaomi Portable Power Bank",
+  //     image: "/accessories/CargadorPortátilXiaomi.webp",
+  //     category: "Accessories",
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "Xiaomi 33W Charger",
+  //     image: "/accessories/CargadorXiaomi.webp",
+  //     category: "Accessories",
+  //   },
+  //   {
+  //     id: 6,
+  //     name: "Smart Band 10 Glacier",
+  //     image: "/accessories/SmartwachXiaomiMiBand10Glaciar.webp",
+  //     category: "Accessories",
+  //   },
+  //   {
+  //     id: 7,
+  //     name: "Redmi Watch 5 Lite",
+  //     image: "/accessories/SmartwatchXiaomiRedmi5Lite.webp",
+  //     category: "Accessories",
+  //   },
+  //   {
+  //     id: 8,
+  //     name: "Watch 5 Active",
+  //     image: "/accessories/SmartwatchXiaomiWatch5Active.webp",
+  //     category: "Accessories",
+  //   },
+  //   {
+  //     id: 9,
+  //     name: "Smart Band 10 Silver",
+  //     image: "/accessories/XiaomiSmartBand10Plateado.webp",
+  //     category: "Accessories",
+  //   },
+
+  //   // --- Phones ---
+  //   // Ubicados en: public/phones/
+  //   {
+  //     id: 10,
+  //     name: "Samsung Galaxy Light Blue",
+  //     image: "/phones/7c25866063c370fa8f69f0c111e816f2.webp",
+  //     category: "Phones",
+  //   },
+  //   {
+  //     id: 11,
+  //     name: "Xiaomi Poco M7",
+  //     image: "/phones/CelularXiaomiPocoM7.webp",
+  //     category: "Phones",
+  //   },
+  //   {
+  //     id: 12,
+  //     name: "Redmi Note 14 Pro",
+  //     image: "/phones/CelularXiaomiRedmiNote14Pro.webp",
+  //     category: "Phones",
+  //   },
+  //   {
+  //     id: 13,
+  //     name: "iPhone 15",
+  //     image: "/phones/Iphone 15.webp",
+  //     category: "Phones",
+  //   },
+  //   {
+  //     id: 14,
+  //     name: "iPhone 16",
+  //     image: "/phones/Iphone 16.webp",
+  //     category: "Phones",
+  //   },
+  //   {
+  //     id: 15,
+  //     name: "iPhone 17",
+  //     image: "/phones/Iphone 17.webp",
+  //     category: "Phones",
+  //   },
+  //   {
+  //     id: 16,
+  //     name: "Samsung Galaxy A23",
+  //     image: "/phones/Samsung Galaxy A23.webp",
+  //     category: "Phones",
+  //   },
+  //   {
+  //     id: 17,
+  //     name: "Samsung Galaxy A56 5G",
+  //     image: "/phones/SAMSUNG-Awesome-Olive-1.webp",
+  //     category: "Phones",
+  //   },
+  //   {
+  //     id: 18,
+  //     name: "Samsung Galaxy Z Flip",
+  //     image:
+  //       "/phones/samsung-galaxy-z-flip-phone-transparent-704081694682521pspdfxugo9.webp",
+  //     category: "Phones",
+  //   },
+  //   {
+  //     id: 19,
+  //     name: "Xiaomi Redmi Note 15",
+  //     image: "/phones/XiaomiNote15.webp",
+  //     category: "Phones",
+  //   },
+  // ];
+
+  return <></>;
+}
+
+export default App;
