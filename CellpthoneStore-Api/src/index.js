548afe0a8b977b7247dffeebf1414a8f41de9e35
Laura-Suez <laura.suez@outlook.com>
2026-04-24 00:30:36 -0300

    Creo API

--- CellpthoneStore-Api/src/index.js
+++ CellpthoneStore-Api/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { PORT } from "./config.js";
+import cellphonesRoutes from "./routes/cellphones.routes.js";
 
 const app = express();
 
 app.listen(PORT);
+app.use(cellphonesRoutes);
 console.log(`Server listening on port ${PORT}`);
