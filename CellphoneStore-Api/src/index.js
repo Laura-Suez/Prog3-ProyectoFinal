8c931a074762c3881e81ec6fcab95483566d55a5
Cristiaanz <cristiaan_rodriguez@hotmail.com>
2026-06-07 21:33:19 -0300

    Se agregan cambios en el backend

--- CellphoneStore-Api/src/index.js
+++ CellphoneStore-Api/src/index.js
@@ -2,15 +2,20 @@ import express from "express";
 import cors from "cors";
 import { PORT } from "./config.js";
 import productsRoutes from "./routes/productsRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { sequelize } from "./db.js";
 import { User, Product, Order } from "./models/models.js";
+
 
 const app = express();
 
 try {
   app.use(express.json());
   app.use(cors());
+
   app.use(productsRoutes);
+  app.use(userRoutes);
+
   app.listen(PORT);
   await sequelize.sync();
 
